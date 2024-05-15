40dd89c2ab90820a4c96b03c8a705fcc2c8948e1
semkufu95 <semkufu95@gmail.com>
2024-05-14 21:18:59 -0700

    New corrections

--- 0x06-regular_expressions/3-repitition_token_2.rb
+++ 0x06-regular_expressions/3-repitition_token_2.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
+#A regular expression for matching
 
 puts ARGV[0].scan(/hbt+n/).join
--- 0x06-regular_expressions/4-repitition_token_3.rb
+++ 0x06-regular_expressions/4-repitition_token_3.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
+#Aregular expression to search for repeating script
 
 puts ARGV[0].scan(/hbt?*n/).join
