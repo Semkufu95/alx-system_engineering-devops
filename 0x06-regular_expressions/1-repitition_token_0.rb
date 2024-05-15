e5acfa8741c2f579436c3931b8b1ed7bc2c40eb7
semkufu95 <semkufu95@gmail.com>
2024-05-14 21:07:54 -0700

    Correction for task 1

--- 0x06-regular_expressions/1-repitition_token_0.rb
+++ 0x06-regular_expressions/1-repitition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #A Ruby script that accepts one argument and pass it to regular expression matching methhod\
 
-puts ARGV[0].scan(hbt{1,5}n).join
+puts ARGV[0].scan(/hbt{1,5}n/).join
