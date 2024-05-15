38312811e687544f9409a798bcb75ae4511b4917
semkufu95 <semkufu95@gmail.com>
2024-05-14 22:30:35 -0700

    filter text from sender receiver and flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #A regex to print text logs
 
-puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\s\[flags:(\S*)\]/).join(',')
+puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)]\s\[flags:(\S*)\]/).join(',')
