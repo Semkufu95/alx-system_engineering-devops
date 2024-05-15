75b95443f28a13f6ada68bcbbfd1850c5ce1a380
semkufu95 <semkufu95@gmail.com>
2024-05-14 22:05:35 -0700

    Show only capital ltters

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #A regular expression that searches for phone numbers of 10 digits
 
-puts ARGV[0].scan(/\d{10}/).join
+puts ARGV[0].scan(/^\d{10}/$).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#A regular expressions that shows only capital letters
+
+puts ARGV[0].scan(/A-Z/).join
