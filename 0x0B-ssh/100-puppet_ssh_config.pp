b05db12ba2da15abafd7be38ec70d8f36307060a
semkufu95 <semkufu95@gmail.com>
2024-07-04 05:32:28 +0300

    Advanced task: without a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Setting up client configuration file to connect to a server without typing in the password
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
