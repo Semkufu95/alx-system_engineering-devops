f5515c67456b0f397149df2a4e615a1119444287
semkufu95 <semkufu95@gmail.com>
2024-07-04 04:49:21 +0300

    Correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,14 @@ package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip3',
 }
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Install python3-pip
+package { 'python3-pip':
+  ensure   => 'present',
+}
