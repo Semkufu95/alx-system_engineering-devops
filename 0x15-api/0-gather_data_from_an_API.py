e9796a8ef1592d1c9b232f8c40547727352ceb8a
semkufu95 <semkufu95@gmail.com>
2024-07-30 13:24:57 +0300

    New codes to fetch data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """
-A script to gather data from an API
+A script that fetches data from an API
 """
+
 import requests
 import sys
 
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", data={"userId": sys.argv[1]}).json()
 
-    complete = [k.get("title") for k in todos if k.get('completed') is True]
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    task = requests.get(url + "todos", data={"userId": (sys.argv[1])}).json()
+    complete = []
+    for t in task:
+        if t.get('completed') is True:
+            complete.append(t.get('title'))
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(complete), len(todos)))
-    [print("\t {}".format(i)) for i in complete]
+        user.get("name"), len(complete), len(task)))
+    for c in complete:
+        print("\t {}".format(c))
