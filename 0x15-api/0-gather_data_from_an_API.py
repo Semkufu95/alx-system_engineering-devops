1825d4a0150f4a8c228d847953aa417d5e3f531c
semkufu95 <semkufu95@gmail.com>
2024-07-30 12:54:59 +0300

    Fetch data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,9 +5,10 @@ A script to gather data from an API
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url +"users/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", data={"userId": sys.argv[1]}).json()
 
     complete = [k.get("title") for k in todos if k.get('completed') is True]
