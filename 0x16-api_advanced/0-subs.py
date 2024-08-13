fb176431c26179f61213d58d63269f9b24ff4a89
semkufu95 <semkufu95@gmail.com>
2024-08-14 01:06:36 +0300

    Fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,11 @@ A function that queries REDDIT API and returns the number of subreddits
 
 import requests
 
+headers =  {'User-Agent': 'python:subscribers.counter:v1:0:0 (by semkufu)'}
+
 
 def number_of_subscribers(subreddit):
-    # set header to avoid too many requests errors
-    headers = {'User-Agent': 'python:subscribers.counter:v1:0:0 (by semkufu)'}
+    """ set header to avoid too many requests errors """
     api_url = f'https://www.reddit.com/r/{subreddit}/about.json'
     result = requests.get(api_url, headers=headers, allow_redirects=False)
 
